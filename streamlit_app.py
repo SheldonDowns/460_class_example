1f9087ab98c13e7acebe500d0aae84cf24f2b38b
SheldonDowns <sheldondowns2002@gmail.com>
2025-03-25 15:40:30 -0600

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ st.subheader("Enter mesurement")
 col1, col2 = st.columns(2)
 
 with col1:
-    feet = st.number_input("Height (feet)", min_value=1, max_value = 8, value=5.0, step=1)
+    feet = st.number_input("Height (feet)", min_value=1, max_value = 8, value=5, step=1)
 
 with col2:
     inches = st.number_input("Height (inches)", min_value=0, max_value = 11, value=6, step=1)
