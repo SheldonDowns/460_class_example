c559ed6fe694f20e76e33663ec7f413056290cd5
SheldonDowns <sheldondowns2002@gmail.com>
2025-03-25 15:38:13 -0600

    Test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,22 @@
 import streamlit as st
+import joblib
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Gender prediction")
+
+@st.cache_resource
+def load_model():
+    return joblib.load("naive_bayes_gender_model_US.pkl")
+
+model = load_model()
+
+st.subheader("Enter mesurement")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    feet = st.number_input("Height (feet)", min_value=1, max_value = 8, value=5.0, step=1)
+
+with col2:
+    inches = st.number_input("Height (inches)", min_value=0, max_value = 11, value=6, step=1)
+
+height = feet (12 /inches)
